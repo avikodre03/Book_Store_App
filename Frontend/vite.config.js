fc153745a8720939118b984b5a8c69956cfe395c
avikodre03 <akodre111@gmail.com>
2024-08-31 14:48:42 +0530

    v3

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,19 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-server:{
-proxy:{
-  '/api':{
-    target: process.env.VITE_API_TARGET,
-    changeOrigin:true,
-    secure:false
-  }
-}
-},
-
-  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5000,
+    proxy: {
+      '/api': {
+        target: import.meta.env.VITE_API_TARGET, 
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  plugins: [react()]
 })
